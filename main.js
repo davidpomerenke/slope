72c7699cb66c7d03ebae2b15631159290cc2374b
David Pomerenke <david.pomerenke@uni-konstanz.de>
2019-04-18 19:10:12 +0200

    clean upstill the four-technique comparison view

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
-var IS_TEST_RUN = true;
-
-// Insert random configuration here:
-// First, numbers 1 to 18 shuffled (custom data),
-// Then, numbers 1 to 7 shuffled (real world data).
-var SEED = [1,2,3,4,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,1,2,3,4,5,6,7];
-
-// Task numbers at which task 2 and 3 start
-var TASK_LENGTH = [2,4];
-
-// max number of clusters which ever appears in a dataset
-var MAX_CLUSTERS = 5;
-
 function getFiles() {
-
+    var SEED = [1,2,3,4,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,1,2,3,4,5,6,7];
     var files = [];
     SEED.forEach(function(value, index) {
         var file;
@@ -31,10 +18,10 @@ function getFiles() {
             }
             else {
                 if (index < 10) {
-                    noise = "90";
+                    noise = "60";
                 } 
                 else if (index < 16) {
-                    noise = "60";
+                    noise = "90";
                 }
                 else {
                     noise = "120";
@@ -55,4 +42,25 @@ Number.prototype.pad = function(size) {
     var s = String(this);
     while (s.length < (size || 2)) {s = "0" + s;}
     return s;
+}
+
+var files = getFiles();
+var i = 0;
+
+function next() {
+    document.getElementById("pcTarget1").innerHTML = "";
+    document.getElementById("pcTarget2").innerHTML = "";
+    document.getElementById("pcTarget3").innerHTML = "";
+    document.getElementById("pcTarget4").innerHTML = "";
+    pcVis(files[i], "#pcTarget1", 0.5, "emphasize_dis");
+    pcVis(files[i], "#pcTarget2", 0.5, "bold");
+    pcVis(files[i], "#pcTarget3", 0.5, "thin");
+    pcVis(files[i], "#pcTarget4", 0.5, "emphasize_sim");
+    if (i < files.length) {
+        i++;
+    }
+    else {
+        alert("All datasets displayed, starting again from set one.")
+        i = 0;
+    }
 }
