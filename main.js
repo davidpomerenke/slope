ced89655ca3aa1954f5916a8535d3acab7e7f0cc
David Pomerenke <david.pomerenke@uni-konstanz.de>
2019-04-19 13:28:44 +0200

    add sliders (without functionality)

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
 function main() {
-    var wrapper = document.getElementById("wrapper");
-        var files = [];
+    
+    ["height", "space", "thickness"].forEach(function(parameter){
+        $("#slider-" + parameter).slider({
+            min: 0,
+            max: 50,
+            value: 20
+        }); 
+    });
+    // jQuery UI slider function
+    
+
+    var wrapper = document.getElementById("main");
+
+    var files = [];
+
     for (i=1; i<4; i++) {
+
         for (j=1; j<=1; j++) {
-            noise = "300";
+
+            noise = "300"; // TODO: different noise levels
+
             files.push("data/synthetic/FinalVersion/Original/" + noise + "N/DefaultOrdering/" + i + "C." + j + ".csv");
+
         }
     }
+
     for (file of [
         "RW03-ecoli-normalized.csv", 
         "RW06-iris-normalized.csv", 
@@ -19,10 +37,14 @@ function main() {
         "RW05-glass-normalized.csv", 
         "RW08-mtCars-normalized.csv"
     ]) { files.push("data/realworld datasets/FinalVersion/" + file); }
-    for (file of files) {
+
+    for (file of files.slice(1, 5)) {
+
         wrapper.appendChild(document.createElement("div")).setAttribute("id", "pcTarget"+i+"a");
         wrapper.appendChild(document.createElement("div")).setAttribute("id", "pcTarget"+i+"b");
+
         pcVis(file, "#pcTarget"+i+"a", "emphasize dis", 0.5, );
         pcVis(file, "#pcTarget"+i+"b", "neutral", 0.5);
+
     }
 }
