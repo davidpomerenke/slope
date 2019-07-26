c254e107ad82852aeaa1f1fa2f87583338b99589
David Pomerenke <david.pomerenke@uni-konstanz.de>
2019-07-26 18:07:03 +0200

    minor changes

--- js/main.js
+++ js/main.js
@@ -1,124 +1,125 @@
-var files = [];
-var activeFiles = new Set();
+var files = []
+var activeFiles = new Set()
 
 function update(parameter, value) {
-    
-    $("#show-" + parameter).text(Math.round(value * 10) / 10);
+
+    $("#show-" + parameter).text(Math.round(value * 10) / 10)
 
     if (parameter === "height") {
-        height = value;
+        height = value
     } else if (parameter === "space") {
-        space = value;
+        space = value
     } else if (parameter === "c") {
-        c = value;
+        c = value
     } else if (parameter === "linewidth") {
-        linewidth = value;
+        linewidth = value
     } else if (parameter === "opacity") {
-        opacity = value+"";
+        opacity = value + ""
     } else if (parameter === "colour") {
-        color = $("#select-colour").val();
+        color = $("#select-colour").val()
     } else if (parameter === "power") {
-        power = value;
+        power = value
     } else if (parameter === "rendering") {
-        rendering = $("#select-rendering").val();
+        rendering = $("#select-rendering").val()
     }
     // Toggle off and on for an update
-    var copy = Array.from(activeFiles);
-    copy.forEach(e => { toggleVis(e); toggleVis(e) } )
+    var copy = Array.from(activeFiles)
+    copy.forEach(e => { toggleVis(e); toggleVis(e) })
 }
 
 function toggleVis(file) {
 
     if (activeFiles.has(file)) {
-        activeFiles.delete(file);
+        activeFiles.delete(file)
     } else {
-        activeFiles.add(file);
+        activeFiles.add(file)
     }
 
     var i = files.map(f => f[0]).indexOf(file);
 
     ["a", "b"].forEach(s => {
 
-        if ($("#pcTarget"+i+s).html() === "") {
-            pcVis(file, "#pcTarget"+i+s, (s === "a" ? "original" : "adjusted"), 0.5);
+        if ($("#pcTarget" + i + s).html() === "") {
+            pcVis(file, "#pcTarget" + i + s, (s === "a" ? "original" : "adjusted"), 0.5)
         }
         else {
-            $("#pcTarget"+i+s).html("");
+            $("#pcTarget" + i + s).html("")
         }
 
-    });
+    })
 }
 
 function main() {
-    
-    $("input:checkbox :not(.linkcheck)").removeAttr("checked").prop("checked", false);
+
+    $("input:checkbox :not(.linkcheck)").removeAttr("checked").prop("checked", false)
 
     $(".linkcheck").attr("onchange", "update()");
 
     // jQuery UI slider function
-    ["height", "space", "c"].forEach(function(parameter){
+    ["height", "space", "c"].forEach(function (parameter) {
         $("#slider-" + parameter).slider({
             step: 0.05,
             min: 0.6,
             max: 1.4,
-            value: 1, 
-            stop: function(event, ui) {
-                update(parameter, ui.value * ui.value);
+            value: 1,
+            stop: function (event, ui) {
+                update(parameter, ui.value * ui.value)
             }
-        }); 
-    });
+        })
+    })
 
     $("#slider-thickness").slider({
         step: 0.1,
         min: 0.1,
         max: 5,
-        value: 1, 
-        stop: function(event, ui) {
-            update("linewidth", ui.value * ui.value * ui.value);
+        value: 1,
+        stop: function (event, ui) {
+            update("linewidth", ui.value * ui.value * ui.value)
         }
-    }); 
+    })
 
     $("#slider-opacity").slider({
         step: 0.01,
         min: 0,
         max: 1,
-        value: 1, 
-        stop: function(event, ui) {
-            update("opacity", ui.value*ui.value*ui.value);
+        value: 1,
+        stop: function (event, ui) {
+            update("opacity", ui.value * ui.value * ui.value)
         }
-    });
+    })
 
     $("#slider-power").slider({
         step: 0.05,
         min: -2,
         max: 5,
         value: 1,
-        stop: function(event, ui) {
-            update("power", ui.value);
+        stop: function (event, ui) {
+            update("power", ui.value)
         }
-    });
+    })
 
     // define headings for datasets
     var headings = {
-            0: "Random Noise", 
-            4: "Linear Noise", //+- 0.2
-            8: "Gaussian Noise", 
-            12: "Synthetic Data", 
-            16: "Variance",
-            20: "Correlations", 
-            24: "Realworld Data",
-            28: "Own Datasets" };
+        0: "Random Noise",
+        4: "Linear Noise", //+- 0.2
+        8: "Gaussian Noise",
+        12: "Synthetic Data",
+        16: "Variance",
+        20: "Correlations",
+        24: "Realworld Data",
+        28: "Own Datasets"
+    }
 
     // add all file names and short descriptions
     for (dataset of ["uniform", "linear", "gaussian", "synthetic", "maxvar", "correlated"]) {
         for (i of [100, 200, 400, 800]) {
-            files.push(["data/"+dataset+"/"+i+".csv", "N"+i])
+            files.push(["data/" + dataset + "/" + i + ".csv", "N" + i])
         }
     }
     for (file of [
         //"RW03-ecoli-normalized.csv", 
         //"RW06-iris-normalized.csv", 
-        ["RW09-seeds-normalized.csv", "Seeds"], 
+        ["RW09-seeds-normalized.csv", "Seeds"],
         ["RW01-airquality-normalized.csv", "Air Quality"],
         ["RW04-forestfires-normalized.csv", "Forest Fires"],
         ["RW07-wine-normalized.csv", "Wine"],
@@ -126,52 +127,52 @@ function main() {
         //"RW02-breasttissue-normalized.csv", 
         //"RW05-glass-normalized.csv", 
         //"RW08-mtCars-normalized.csv"
-    ]) { 
-        files.push(["data/realworld/" + file[0], file[1]]); 
+    ]) {
+        files.push(["data/realworld/" + file[0], file[1]])
     }
 
     for (ownfile of ownFiles) {
-        files.push(["data/own/"+ownfile[0], ownfile[1]]);
+        files.push(["data/own/" + ownfile[0], ownfile[1]])
     }
 
-    var i = 0; 
-    
+    var i = 0
+
     // create headers for checkboxes and target areas for plots
     for (file of files) {
-            
-        if (Object.keys(headings).includes(i+"")) {
+
+        if (Object.keys(headings).includes(i + "")) {
             var checkboxgroup = $("<section>")
                 .attr("class", "checkboxgroup")
-                .appendTo("#boxes");
+                .appendTo("#boxes")
             $("<b>")
                 .text(headings[i])
-                .appendTo(checkboxgroup);
+                .appendTo(checkboxgroup)
         }
 
         // create checkboxes
         var section = $("<section>")
-            .appendTo(checkboxgroup);
+            .appendTo(checkboxgroup)
         $("<input>")
             .attr("type", "checkbox")
             .attr("id", file[0])
-            .attr("onChange", "toggleVis('"+file[0]+"')")
-            .appendTo(section);
+            .attr("onChange", "toggleVis('" + file[0] + "')")
+            .appendTo(section)
         $("<label>")
             .attr("for", file[0])
             .text(file[1])
-            .appendTo(section);
+            .appendTo(section)
 
         // create targets for ordered figures
         var comparebox = $("<div>")
             .attr("class", "row")
-            .appendTo("main");
+            .appendTo("main")
         $("<div>")
-            .attr("id", "pcTarget"+i+"a")
-            .appendTo(comparebox);
+            .attr("id", "pcTarget" + i + "a")
+            .appendTo(comparebox)
         $("<div>")
-            .attr("id", "pcTarget"+i+"b")
-            .appendTo(comparebox);
+            .attr("id", "pcTarget" + i + "b")
+            .appendTo(comparebox)
 
-        i++;
+        i++
     }
 }
--- js/visualization.js
+++ js/visualization.js
@@ -1,12 +1,12 @@
-var height = 1.0;
-var space = 1.0;
-var linewidth = 1.0;
-var opacity = 1.0;
-var power = 1.0;
-var color = "black";
-var scale = 1.0;
-var c = 1.0;
-var rendering ="line";
+var height = 1.0
+var space = 1.0
+var linewidth = 1.0
+var opacity = 1.0
+var power = 1.0
+var color = "black"
+var scale = 1.0
+var c = 1.0
+var rendering ="line"
 
 function adjustment // for a couple of points on two neighbouring axes
 (
@@ -15,34 +15,34 @@ function adjustment // for a couple of points on two neighbouring axes
     widthDifference // between the points = between the axes
 ) {
     if (method === "original") {
-        return 1;
+        return 1
     } 
     
     else if (method === "adjusted") {
 
-		var alpha = Math.abs(Math.atan(heightDifference / widthDifference));
-        return Math.pow(Math.cos(alpha), power) * c;
+		var alpha = Math.abs(Math.atan(heightDifference / widthDifference))
+        return Math.pow(Math.cos(alpha), power) * c
     } 
     
     else { // experimental techniques, unused here
     
-        alpha = Math.abs(Math.atan(heightDifference / widthDifference));
+        alpha = Math.abs(Math.atan(heightDifference / widthDifference))
         
-        maxHeightDifference = Math.abs(y_scale(1) - y_scale(0));
+        maxHeightDifference = Math.abs(y_scale(1) - y_scale(0))
         
-        alpha_max = Math.abs(Math.atan(maxHeightDifference / widthDifference));
+        alpha_max = Math.abs(Math.atan(maxHeightDifference / widthDifference))
 
         if (method === "neutral bold") {
-            return Math.abs(Math.cos(alpha)) / Math.abs(Math.cos(alpha_max)) / 2;
+            return Math.abs(Math.cos(alpha)) / Math.abs(Math.cos(alpha_max)) / 2
         }
 
         else if (method === "emphasize sim") {
-            return Math.abs(Math.cos(alpha)) / Math.abs(Math.cos(alpha_max - alpha));
+            return Math.abs(Math.cos(alpha)) / Math.abs(Math.cos(alpha_max - alpha))
         }
 
         else {
-            console.log("Error: Invalid line method specified.");
-            return 0;
+            console.log("Error: Invalid line method specified.")
+            return 0
         }
     }
 }
@@ -50,22 +50,22 @@ function adjustment // for a couple of points on two neighbouring axes
 /* lineMethod
  - original:                     normal lines
  - adjusted:                     adjust line width with math
- - polygon:                      adjust line width with polygon; poorer anti-aliasing
- - neutral bold  (experimental): neutral; line width corresponds to boldest possible line in emphasize dis
- - emphasize sim (experimental): revert emphasize dis effect; useless */
+ - polygon:                      adjust line width with polygon poorer anti-aliasing
+ - neutral bold  (experimental): neutral line width corresponds to boldest possible line in emphasize dis
+ - emphasize sim (experimental): revert emphasize dis effect useless */
 function pcVis(file, pcTarget, method) {
 
     var m = [10 * height * scale, 10 * space * scale, 10 * height * scale, 10 * space * scale],
         w = (480 - m[1] - m[3]) * scale * space,
-        h = (250 - m[0] - m[2]) * scale * height;
+        h = (250 - m[0] - m[2]) * scale * height
 
     var x = d3.scale.ordinal().rangePoints([0, w], 1),
         y = {},
-        dragging = {};
+        dragging = {}
 
     var line = d3.svg.line(),
         axis = d3.svg.axis().orient("left"),
-        foreground;
+        foreground
 
     var svg = d3.select(pcTarget)
         .append("svg:svg")
@@ -75,33 +75,33 @@ function pcVis(file, pcTarget, method) {
             .append("svg:g")
                 .attr("transform", "translate(" + m[1] + "," + m[0] + "), scale(" + scale + ")")
                 .attr("draggable", "false")
-                .attr("class", "box");
+                .attr("class", "box")
 
     d3.csv(file, function (data) {
         
-        $(pcTarget).css("display", "none");
+        $(pcTarget).css("display", "none")
         
         y_scale = d3.scale.linear()
                 .domain([0, 1])
-                .range([h, 0]);
+                .range([h, 0])
 
         // Extract the list of dimensions and create a scale for each.
         x.domain(dimensions = d3.keys(data[0]).filter(function (d) {
             return d !== "ObjectID" && d !== "id" && d !== "ObjectId" && d != "Cluster" && d !== "cluster" && (y[d] = d3.scale.linear()
                 .domain([0, 1])
-                .range([h, 0]));
-        }));
+                .range([h, 0]))
+        }))
 
-        var categories = new Set();
+        var categories = new Set()
         data.forEach(d => {
-            categories.add(d[Object.keys(d)[0]]);
-        });
-        colors = ["grey", "blue", "red", "green", "turquoise", "violet", "yellow"];
-        colormap = {};
-        i = 0;
+            categories.add(d[Object.keys(d)[0]])
+        })
+        colors = ["grey", "blue", "red", "green", "turquoise", "violet", "yellow"]
+        colormap = {}
+        i = 0
         for (cat of categories) {
-            colormap[cat] = colors[i % colors.length];
-            i++;
+            colormap[cat] = colors[i % colors.length]
+            i++
         }
         
         // Returns the path for a given data point.
@@ -109,31 +109,31 @@ function pcVis(file, pcTarget, method) {
         function createPathFunction(k) {
             return function(d) {
                 points = [dimensions[k],dimensions[k+1]].map(function (p) {
-                    return [position(p), y_scale(d[p])];
-                });
-                return line(points);  
+                    return [position(p), y_scale(d[p])]
+                })
+                return line(points)  
             }
         }
 
         function createAdjustmentFunction(k, factor) {
             return function(d) {
                 points = [dimensions[k],dimensions[k+1]].map(function (p) {
-                    return [position(p), y_scale(d[p])];
-                });
-                heightDifference = Math.abs(points[1][1] - points[0][1]);
-                widthDifference = Math.abs(points[1][0] - points[0][0]);
+                    return [position(p), y_scale(d[p])]
+                })
+                heightDifference = Math.abs(points[1][1] - points[0][1])
+                widthDifference = Math.abs(points[1][0] - points[0][0])
 
-                return adjustment(method, heightDifference, widthDifference) * factor;
+                return adjustment(method, heightDifference, widthDifference) * factor
             }
         }
 
         function createPolygonFunction(k, factor) {
             return function(d) {
                 points = [dimensions[k],dimensions[k+1]].map(function (p) {
-                    return [position(p), y_scale(d[p])];
-                });
-                heightDifference = Math.abs(points[1][1] - points[0][1]);
-                widthDifference = Math.abs(points[1][0] - points[0][0]);
+                    return [position(p), y_scale(d[p])]
+                })
+                heightDifference = Math.abs(points[1][1] - points[0][1])
+                widthDifference = Math.abs(points[1][0] - points[0][0])
                 return ""
                     + points[0][0] + "," +  points[0][1]      + " "
                     + points[0][0] + "," + (points[0][1] + factor) + " "
@@ -144,18 +144,18 @@ function pcVis(file, pcTarget, method) {
 
         function createColourFunction(k) {
             return function(d) {
-                return colormap[d[Object.keys(d)[0]]];
+                return colormap[d[Object.keys(d)[0]]]
             }
         }
         
-        foreground = [];
+        foreground = []
 
         for (k = 0; k < (dimensions.length - 1); k++) {
 
             // Add blue foreground lines
             foreground[k] = svg.append("svg:g")
                 .attr("class", "foreground")
-                .attr("id", "fground"+k);
+                .attr("id", "fground"+k)
 
             if (method === "original" || rendering === "line") {
                 foreground[k].selectAll("path")
@@ -175,7 +175,7 @@ function pcVis(file, pcTarget, method) {
                                 ? createColourFunction(k) 
                                 : color)
                             .attr("shape-rendering", "geometricPrecision")
-                            .attr("fill", "none");
+                            .attr("fill", "none")
             }
             else if (method === "adjusted" && rendering === "polygon") {
                 // alternative rendering with polygon and without math
@@ -203,26 +203,26 @@ function pcVis(file, pcTarget, method) {
             .enter().append("svg:g")
             .attr("class", "dimension")
             .attr("transform", function (d) {
-                return "translate(" + x(d) + ")";
-            });
+                return "translate(" + x(d) + ")"
+            })
 
         // Add an axis.
         g.append("svg:g")
             .attr("class", "axis")
             .each(function (d) {
-                d3.select(this).call(axis.scale(y[d]).ticks(0));
-            });
+                d3.select(this).call(axis.scale(y[d]).ticks(0))
+            })
 
         $(".domain")
             .attr("stroke", "black")
             .attr("fill", "none")
-            .attr("shape-rendering", "geometricPrecision");
+            .attr("shape-rendering", "geometricPrecision")
         
-        $(pcTarget).css("display", "unset");
-    });
+        $(pcTarget).css("display", "unset")
+    })
 
     function position(d) {
-        var v = dragging[d];
-        return v == null ? x(d) : v;
+        var v = dragging[d]
+        return v == null ? x(d) : v
     }
 }
